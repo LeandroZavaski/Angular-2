e46dd89e16b78ff09116ab3f1d054ba78cadaf41
Leandro Cezar Zavaski <lczavaski@scopus.com.br>
2017-12-28 13:58:32 -0200

    Atualizações

---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent {
+  nomePortal: string;
+  cursos : string[];
+
+  constructor(public cursosService: CursosService) {
+    this.nomePortal = 'http://www.google.com.br';
+    this.cursos = this.cursosService.getCursos();
+    /* var servico = new CursosService(); */
+
+  }
+}
